1bdd97d7608b666b7e6bcd71621953e6205bf563
l1nkolnkr <mr.aleks111@gmail.com>
2022-04-27 22:48:53 +0300

    useState practice

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import {Accordion} from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
 
 function App() {
     return (
         <div className="App">
-            <PageTitle title={"this is app"}/>
-            <Rating value={1}/>
-            <Rating value={4}/>
-            <Accordion titleValue={"menu"} collapsed={true}/>
-            <Accordion titleValue={"main"} collapsed={false}/>
+            <OnOff />
+            {/*<PageTitle title={"this is app"}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Accordion titleValue={"menu"} collapsed={true}/>*/}
+            {/*<Accordion titleValue={"main"} collapsed={false}/>*/}
         </div>
     );
 }
